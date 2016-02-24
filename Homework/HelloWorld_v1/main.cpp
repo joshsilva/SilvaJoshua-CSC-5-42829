3e752a082467b27af0dda5100579264bab8744fb
Joshua Silva <joshss2015@gmail.com>
2016-02-24 08:06:42 -0800

    Deleted zip abd added Hello World

---
+++ Homework/HelloWorld_v1/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Joshua Silva
+ * Created on February 17, 2016, 9:45 AM
+ * Purpose: Our First Program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+ //User Libraries
+     
+ // Global Constants
+     
+ //Function Prototypes
+
+ //Execution Begins Here!    
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Input Values
+    
+    //Map the inputs to the outputs
+    
+    //Output the results
+    cout<<"Hello World"<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
+
