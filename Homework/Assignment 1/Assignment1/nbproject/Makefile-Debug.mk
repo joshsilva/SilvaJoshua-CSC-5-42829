5129401fbb5e2ce213cbbb4f5e0ca41b4cb85adb
Joshua Silva <Joshua Silva>
2016-03-04 15:34:02 -0800

    Signed-off-by: Joshua Silva <Joshua Silva>

--- Homework/Assignment 1/Assignment1/nbproject/Makefile-Debug.mk
+++ Homework/Assignment 1/Assignment1/nbproject/Makefile-Debug.mk
@@ -23,7 +23,7 @@ AS=as
 # Macros
 CND_PLATFORM=Cygwin_1-Windows
 CND_DLIB_EXT=dll
-CND_CONF=Release
+CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 
@@ -65,7 +65,7 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/assignment1.exe: ${OBJECTFILES}
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
