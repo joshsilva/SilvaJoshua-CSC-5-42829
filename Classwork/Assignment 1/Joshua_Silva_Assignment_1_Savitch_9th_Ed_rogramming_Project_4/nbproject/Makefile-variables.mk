2a0b61cc1ec2692772fdf5f52130d46fe5995b34
Joshua Silva <Joshua Silva>
2016-03-04 19:13:08 -0800

    Signed-off-by: Joshua Silva <Joshua Silva>

---
+++ Classwork/Assignment 1/Joshua_Silva_Assignment_1_Savitch_9th_Ed_rogramming_Project_4/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_1-Windows
+CND_ARTIFACT_NAME_Debug=joshua_silva_assignment_1_savitch_9th_ed_rogramming_project_4
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_1-Windows/joshua_silva_assignment_1_savitch_9th_ed_rogramming_project_4
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_1-Windows/package
+CND_PACKAGE_NAME_Debug=joshuasilvaassignment1savitch9thedrogrammingproject4.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_1-Windows/package/joshuasilvaassignment1savitch9thedrogrammingproject4.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_1-Windows
+CND_ARTIFACT_NAME_Release=joshua_silva_assignment_1_savitch_9th_ed_rogramming_project_4
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/joshua_silva_assignment_1_savitch_9th_ed_rogramming_project_4
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_1-Windows/package
+CND_PACKAGE_NAME_Release=joshuasilvaassignment1savitch9thedrogrammingproject4.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/joshuasilvaassignment1savitch9thedrogrammingproject4.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ Homework/Assignment 1/Assignment1/Joshua_Silva_Assignment_1_Savitch_9th_Ed_Programming_Project_4
@@ -0,0 +1,29 @@
+/*
+ * File:   Programming Project 3
+ * Author: Joshua Silva
+ *
+ * Created on March 4, 2016, 6:11 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Sec, Dist;
+    
+    cout << "Press Enter After Inputing Number\n";
+    cout << "Enter Number of Seconds\n";
+    cin >> Sec;
+    Dist= 32*(Sec*Sec)/2;
+    cout << "If an object falls for ";
+    cout << Sec;
+    cout << " seconds,\n";
+    cout << "the object would have fallen\n";
+    cout << Dist;
+    cout << " feet.";
+    
+    return 0;
+}
+
